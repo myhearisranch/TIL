fb45e54bbf2d36915c5f3144f6c4c180178c1c5e
root <root@DESKTOP-2O6QUN5.localdomain>
2022-08-12 22:49:55 +0900

    フィルタを使い重複を避ける

--- Genba_Rails/taskleaf/app/controllers/tasks_controller.rb
+++ Genba_Rails/taskleaf/app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
-    @tasks = current_user.tasks
+    @tasks = current_user.tasks.order(created_at: :desc)
   end
 
   def show
-    @task = current_user.tasks.find(params[:id])
+   
   end
 
   def new
@@ -12,17 +14,17 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = current_user.tasks.find(params[:id])
+   
   end
 
   def update
-    task = current_user.tasks.find(params[:id])
+    
     task.update(task_params)
     redirect_to tasks_url, notice: "タスク#{task.name}を更新しました。"
   end
 
   def destroy
-    task = current_user.tasks.find(params[:id])
+   
     task.destroy
     # task.name => 別の部分で使う => インスタンス変数(@task.name)ではないのか?
     redirect_to tasks_url, notice: "タスク#{task.name}を削除しました。"
@@ -41,6 +43,10 @@ private
 
   def task_params
     params.require(:task).permit(:name, :description)
+  end
+
+  def set_task
+    @task = current_user.tasks.find(params[:id])
   end
 
 end
