6d88c67f4fc757f4c3f9326edd51ad52344053b9
root <root@DESKTOP-2O6QUN5.localdomain>
2022-08-12 22:34:00 +0900

    scopeを活用する

--- Genba_Rails/taskleaf/app/models/task.rb
+++ Genba_Rails/taskleaf/app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
     validates :name, presence: true
     validates :name,    length: { minimum: 5} 
-
     belongs_to :user
+    scope :recent, -> { order(created_at: desc) }
 end
