6d0c988233b091893feb07756bf2943b3fa34d07
root <root@DESKTOP-2O6QUN5.localdomain>
2022-08-25 22:16:57 +0900

    一覧画面に検索機能を追加する

--- Genba_Rails/taskleaf/app/models/task.rb
+++ Genba_Rails/taskleaf/app/models/task.rb
@@ -2,5 +2,13 @@ class Task < ApplicationRecord
     validates :name, presence: true
     validates :name, presence: true
     belongs_to :user
-    scope :recent, -> { order(created_at: desc) }
+    scope :recent, -> { order(created_at: :desc) }
+
+    def self.ransackable_attributes(auth_object = nil)
+        %w[name created_at]
+    end
+
+    def self.ransackable_associations(auth_object = nil)
+        []
+    end
 end
